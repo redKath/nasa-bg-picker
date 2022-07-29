d159184bad4c30cd8a1e1181c1a8c3015b09b050
Kathleen Jogno <jognokath@gmail.com>
2022-07-29 13:07:07 +0800

    Added styles to layer images

--- js/main.js
+++ js/main.js
@@ -6,23 +6,30 @@ document.querySelector('#speckledSField').addEventListener('click', showSpeckled
 
 
 function showCloudNebula() {
-    document.querySelector('body').style.background = 'url("./images/cloudscape nebula.png") no-repeat center center fixed'
-    document.querySelector('body').style.backgroundSize = 'cover'
+    document.querySelector('body').style.cssText = 'background-image: url("./images/cockpit.png"), url("./images/cloudscape nebula.png") ; background-repeat: no-repeat, no-repeat; background-position: center center, center center; '
+    // document.querySelector('body').style.backgroundImage = 'url("./images/cloudscape nebula.png"), url("./images/cockpit.png")'
+    // document.querySelector('body').style.backgroundRepeat = 'no-repeat, no-repeat'
+    // document.querySelector('body').style.position = 'center center, center center'
+    // document.querySelector('body').style.backgroundSize = 'cover, cover'
 }
 
 function showGalaxyClusters() {
-    document.querySelector('body').style.background = 'url("./images/galaxy clusters.png") no-repeat center center fixed'
-    document.querySelector('body').style.backgroundSize = 'cover'
+    document.querySelector('body').style.cssText = 'background-image: url("./images/cockpit.png"), url("./images/galaxy clusters.png") ; background-repeat: no-repeat, no-repeat; background-position: center center, center center; '
+    // document.querySelector('body').style.background = 'url("./images/galaxy clusters.png") no-repeat center center fixed'
+    // document.querySelector('body').style.backgroundSize = 'cover'
 }
 function showPentaGalaxies() {
-    document.querySelector('body').style.background = 'url("./images/penta galaxies.png") no-repeat center center fixed'
-    document.querySelector('body').style.backgroundSize = 'cover'
+    document.querySelector('body').style.cssText = 'background-image: url("./images/cockpit.png"), url("./images/penta galaxies.png") ; background-repeat: no-repeat, no-repeat; background-position: center center, center center; '
+    // document.querySelector('body').style.background = 'url("./images/penta galaxies.png") no-repeat center center fixed'
+    // document.querySelector('body').style.backgroundSize = 'cover'
 }
 function showSouthernRNebula() {
-    document.querySelector('body').style.background = 'url("./images/southern ring nebula.png") no-repeat center center fixed'
-    document.querySelector('body').style.backgroundSize = 'cover'
+    document.querySelector('body').style.cssText = 'background-image: url("./images/cockpit.png"), url("./images/southern ring nebula.png") ; background-repeat: no-repeat, no-repeat; background-position: center center, center center; '
+    // document.querySelector('body').style.background = 'url("./images/southern ring nebula.png") no-repeat center center fixed'
+    // document.querySelector('body').style.backgroundSize = 'cover'
 }
 function showSpeckledSField() {
-    document.querySelector('body').style.background = 'url("./images/speckled star field.png") no-repeat center center fixed'
-    document.querySelector('body').style.backgroundSize = 'cover'
+    document.querySelector('body').style.cssText = 'background-image: url("./images/cockpit.png"), url("./images/speckled star field.png") ; background-repeat: no-repeat, no-repeat; background-position: center center, center center; '
+    // document.querySelector('body').style.background = 'url("./images/speckled star field.png") no-repeat center center fixed'
+    // document.querySelector('body').style.backgroundSize = 'cover'
 }
